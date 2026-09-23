3050ec8ba9e959642195ba73c8e8884d1f9fecc3
Riccardo Martinelli <rikiper95@gmail.com>
2026-09-23 10:40:57 +0200

    Update and rename R_1st_script.R to 01_R_1st_script.R

---
+++ Code/01_R_1st_script.R
@@ -0,0 +1,15 @@
+#Script for first R use
+
+2+3
+#create an object; comments are used to make scripts more understandable
+samuele <- 2+3
+
+#new object
+gemma <- 4+6
+# different objects can be used for additional operations
+samuele+gemma
+samuele*gemma
+samuele^gemma
+tma <- 5*4
+tma+samuele+gemma
+#objects can be used in complex operations
